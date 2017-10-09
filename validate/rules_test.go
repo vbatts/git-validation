d9bf419b441e9409c6b61835c93757660f134126
Vincent Batts <vbatts@hashbangbash.com>
2017-10-09 13:21:23 -0400

    validate: prep for rules that have a value
    where needed, some rules may need to pass in a value like for a reguler
    expression.
    
    Signed-off-by: Vincent Batts <vbatts@hashbangbash.com>

---
+++ validate/rules_test.go
@@ -0,0 +1,52 @@
+package validate
+
+import (
+	"testing"
+)
+
+func TestSanitizeRules(t *testing.T) {
+	set := []struct {
+		input  string
+		output []string
+	}{
+		{
+			input:  "apples, oranges , bananas",
+			output: []string{"apples", "oranges", "bananas"},
+		},
+		{
+			input:  "apples, oranges , bananas, peaches='with cream'",
+			output: []string{"apples", "oranges", "bananas", "peaches='with cream'"},
+		},
+	}
+
+	for i := range set {
+		filt := SanitizeFilters(set[i].input)
+		if !StringsSliceEqual(filt, set[i].output) {
+			t.Errorf("expected output like %v, but got %v", set[i].output, filt)
+		}
+	}
+}
+
+func TestSliceHelpers(t *testing.T) {
+	set := []struct {
+		A, B  []string
+		Equal bool
+	}{
+		{
+			A:     []string{"apples", "bananas", "oranges", "mango"},
+			B:     []string{"oranges", "bananas", "apples", "mango"},
+			Equal: true,
+		},
+		{
+			A:     []string{"apples", "bananas", "oranges", "mango"},
+			B:     []string{"waffles"},
+			Equal: false,
+		},
+	}
+	for i := range set {
+		got := StringsSliceEqual(set[i].A, set[i].B)
+		if got != set[i].Equal {
+			t.Errorf("expected %d A and B comparison to be %q, but got %q", i, set[i].Equal, got)
+		}
+	}
+}
